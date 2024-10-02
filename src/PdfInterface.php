89b1594d34200be1920e5a4ca919dbbbbf4027b3
Fabiano Mendes <fabianomendesoficial@gmail.com>
2024-10-01 23:41:43 -0300

    Update

---
+++ src/PdfInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+	
+namespace Escolarte\PhpPdftk;
+
+interface PdfInterface
+{
+	
+	public function __construct(string|null $pdf, array $options);
+	
+	public function __get(string $name): mixed;
+	
+	public function __set(string $option, mixed $value): void;
+	
+	public function fillForm(array $data): Pdf;
+	
+	public function flatten(): Pdf;
+	
+	public function execute(): bool;
+	
+	public function saveAs(string $output): bool;
+	
+	public function getTpmFIle(): string;
+}
